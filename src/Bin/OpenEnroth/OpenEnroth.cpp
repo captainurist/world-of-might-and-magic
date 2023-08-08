d02043fc44196323e55b6682c085a568b16e3a7b
captainurist <73941350+captainurist@users.noreply.github.com>
2023-08-08 16:06:40 +0800

    Test ranges build

--- src/Bin/OpenEnroth/OpenEnroth.cpp
+++ src/Bin/OpenEnroth/OpenEnroth.cpp
@@ -18,6 +18,8 @@
 
 #include "OpenEnrothOptions.h"
 
+#include <ranges>
+
 int runRetrace(OpenEnrothOptions options) {
     GameStarter starter(options);
     starter.config()->resetForTest();
@@ -49,6 +51,10 @@ int runRetrace(OpenEnrothOptions options) {
 }
 
 int runOpenEnroth(OpenEnrothOptions options) {
+    const std::vector<int> v = {1, 2, 3};
+    for (int i : v | std::views::reverse)
+        fmt::println("{}", i);
+
     setErrorHandler([](const std::string &title, const std::string &message) {
         if (platform)
             platform->showMessageBox(title, message);
